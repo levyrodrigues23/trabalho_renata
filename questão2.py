cf23c53eeb53731eb80070dc047248136684f5f1
Thaís Ávila Aguiar <thaisaguiar019@gmail.com>
2025-07-11 09:17:10 -0300

    Update questão2.py

--- questão2.py
+++ questão2.py
@@ -6,15 +6,14 @@ jogar_novamente = "s"
 while jogar_novamente == "s":
   jogadas_permitidas = ("pedra", "papel", "tesoura")
   jogada_usuario = input("Digite pedra, papel ou tesoura \n").lower()
-  for i in jogadas_permitidas:
-    if jogada_usuario not in jogadas_permitidas:
+  while jogada_usuario not in jogadas_permitidas:
       print("\nVocê digitou errado")
       jogada_usuario = input("Digite pedra, papel ou tesoura \n").lower()
 
-  jogada_computador = random.randint(0,2)
+  jogada_computador = jogadas_permitidas[random.randint(0,2)]
 
   print(f"\nA sua jogada foi {jogada_usuario}")
-  print(f"A jogada do computador foi {jogadas_permitidas[jogada_computador]}\n")
+  print(f"A jogada do computador foi {jogada_computador}\n")
 
   #Possibilidades em que o usuário ganhou:
   usuario_ganhou = jogada_usuario == "papel" and jogada_computador==0 or jogada_usuario == "pedra" and jogada_computador==2 or jogada_usuario == "tesoura" and jogada_computador == 1
