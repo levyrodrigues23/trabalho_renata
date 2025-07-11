57c55f399fc52e2cf409ba248aca9f555aac8911
Thaís Ávila Aguiar <thaisaguiar019@gmail.com>
2025-07-11 09:18:17 -0300

    Update questão2implementação1.py

--- questão2implementação1.py
+++ questão2implementação1.py
@@ -5,19 +5,18 @@ print(f"\nOlá {nome_de_usuario}!")
 print("Vamos jogar pedra papel ou tesoura!\n")
 
 jogar_novamente = "s"
-
 while jogar_novamente == "s":
   jogadas_permitidas = ("pedra", "papel", "tesoura")
   jogada_usuario = input("Digite pedra, papel ou tesoura \n").lower()
-  for i in jogadas_permitidas:
-    if jogada_usuario not in jogadas_permitidas:
-      print("\nAcho que você digitou errado ):")
-      jogada_usuario = input("Digite pedra, papel ou tesoura \n").lower()
+  while novamente != "s" and novamente != "n":
+      print("Você digitou algo errado! ):")
+      novamente = input("Digite S ou N: ").lower()
 
-  jogada_ptp = random.randint(0,2)
+  jogada_ptp = jogadas_permitidas[random.randint(0,2)]
+
 
   print(f"\nA sua jogada foi {jogada_usuario}")
-  print(f"A minha jogada foi {jogadas_permitidas[jogada_ptp]}\n")
+  print(f"A minha jogada foi {jogada_ptp}\n")
 
   #Possibilidades em que o usuário ganhou:
   usuario_ganhou = jogada_usuario == "papel" and jogada_ptp==0 or jogada_usuario == "pedra" and jogada_ptp==2 or jogada_usuario == "tesoura" and jogada_ptp == 1
@@ -32,7 +31,7 @@ while jogar_novamente == "s":
     print("Parabéns! Você ganhou!")
   else:
     print("Eu ganhei!")
-  
+
   novamente = input("\nDeseja jogar novamente? \nDigite S ou N: ").lower()
   while novamente != "s" and novamente != "n":
       print("Acho que você digitou algo errado! ):")
